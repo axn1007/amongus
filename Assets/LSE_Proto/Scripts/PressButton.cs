ce3194ff61e5f88fd744728cb2595185f1966a65
leesungeon <79202877+leesungeon@users.noreply.github.com>
2021-05-27 17:36:46 +0900

    Fix VR ver

--- Assets/LSE_Proto/Scripts/PressButton.cs
+++ Assets/LSE_Proto/Scripts/PressButton.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class PressButton : MonoBehaviour
 {
-    public Camera getCamera;
+    LineRenderer lr;
+
     private RaycastHit hit;
 
     public float pressTime = 5.0f;
@@ -12,16 +13,42 @@ public class PressButton : MonoBehaviour
 
     void Start()
     {
-        
+        lr = GetComponent<LineRenderer>();
     }
 
     void Update()
     {
-        if (Input.GetMouseButton(0))
-        {
-            Ray ray = getCamera.ScreenPointToRay(Input.mousePosition);
+        DrawGuideLine();
+        PressButtons();
+    }
 
-            if(Physics.Raycast(ray, out hit))
+    void DrawGuideLine()
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            lr.SetPosition(0, transform.position);
+            lr.SetPosition(1, hit.point);
+        }
+
+        else
+        {
+            lr.SetPosition(0, transform.position);
+            lr.SetPosition(1, transform.position + transform.forward * 1);
+        }
+    }
+
+    void PressButtons()
+    {
+        if (Input.GetMouseButton(0) || OVRInput.Get(OVRInput.Button.Two, OVRInput.Controller.RTouch))
+        {
+            //Ray ray = getCamera.ScreenPointToRay(Input.mousePosition);
+            Ray ray = new Ray(transform.position, transform.forward);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
             {
                 curTime += Time.deltaTime;
                 string objectName = hit.collider.gameObject.name;
@@ -31,8 +58,11 @@ public class PressButton : MonoBehaviour
                     print(objectName + " Mission Complete!");
                     MissionManager.instance.ButtonMission(1);
                     curTime = 0;
-                }        
+                    Destroy(hit.collider.gameObject);
+                }
             }
         }
     }
+
+
 }
